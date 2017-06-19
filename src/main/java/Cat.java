934a075b14143e7644dc0cddd858681670dac588
Sergey Kuznetsov <SKuznetsov@idbs.com>
2017-06-19 21:27:49 +0300

    add cat name

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,5 +1,11 @@
 public class Cat {
-    /*
-    * asfaa
-    * */
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
